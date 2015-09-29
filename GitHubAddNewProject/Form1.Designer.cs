85b2dc363ad322f413037aaa0de3574c93cb6597
Trenton Mcleod <tcloud81915@gmail.com>
2015-09-29 07:15:00 -0400

    Mcleod's was moved

--- GitHubAddNewProject/Form1.Designer.cs
+++ GitHubAddNewProject/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 148);
+            this.label2.Location = new System.Drawing.Point(223, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 1;
