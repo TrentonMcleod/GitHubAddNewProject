769c458f52c7b041d3dd179250d2be342179ccce
Trenton Mcleod <tcloud81915@gmail.com>
2015-09-29 07:11:45 -0400

    Trenton is deleted

--- GitHubAddNewProject/Form1.Designer.cs
+++ GitHubAddNewProject/Form1.Designer.cs
@@ -28,18 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Trenton\'s";
             // 
             // label2
             // 
@@ -56,9 +46,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tren";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +55,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
 }
