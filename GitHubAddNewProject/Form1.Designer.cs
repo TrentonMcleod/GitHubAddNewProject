beadb3a96fab29b480a3dd7b2a8d4f0d1e718389
Trenton Mcleod <tcloud81915@gmail.com>
2015-09-29 07:05:57 -0400

    Mcleod is now Mcleod's

--- GitHubAddNewProject/Form1.Designer.cs
+++ GitHubAddNewProject/Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(41, 148);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Mcleod";
+            this.label2.Text = "Mcleod\'s";
             // 
             // Form1
             // 
